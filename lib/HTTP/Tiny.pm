083060be39ec0a1af6ac7c3f73675b51a3277ca7
David Golden <dagolden@cpan.org>
2011-01-13 15:00:09 -0500

    inline the protocol version of requests

--- lib/HTTP/Tiny.pm
+++ lib/HTTP/Tiny.pm
@@ -791,12 +791,10 @@ sub read_response_header {
 }
 
 sub write_request_header {
-    @_ == 4 || @_ == 5 || croak(q/Usage: $handle->write_request_header(method, request_uri, headers [, protocol])/);
-    my ($self, $method, $request_uri, $headers, $protocol) = @_;
+    @_ == 4 || croak(q/Usage: $handle->write_request_header(method, request_uri, headers)/);
+    my ($self, $method, $request_uri, $headers) = @_;
 
-    $protocol ||= 'HTTP/1.1'; # XXX never provided -- dagolden, 2010-12-03
-
-    return $self->write("$method $request_uri $protocol\x0D\x0A")
+    return $self->write("$method $request_uri HTTP/1.1\x0D\x0A")
          + $self->write_header_lines($headers);
 }
 
